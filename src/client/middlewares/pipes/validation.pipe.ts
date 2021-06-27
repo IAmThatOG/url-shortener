fde0e6e5532c87c62a47197a1013a1007cf8ad08
IAmThatG <gabbygabbylexy@gmail.com>
2021-06-27 03:20:21 +0100

    implemented validation pipe for central dto validation handler

---
+++ src/client/middlewares/pipes/validation.pipe.ts
@@ -0,0 +1,22 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform {
+  async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
+    let { metatype } = metadata;
+    if (!metatype || !this.shouldValidate(metatype)) return value;
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      throw new BadRequestException(errors);
+    }
+    return value;
+  }
+
+  private shouldValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
